3e8c0fdc0da338e00cfcb90b7550fbb17341f155
Muhammad Qadri <muhammad.qadri@skipthedishes.ca>
2022-08-16 14:21:46 -0500

    code update to include start session logic

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
-# from crypt import methods
-# from email import message
-from turtle import title
-from flask import Flask, request, render_template, jsonify, session, make_response
+from re import M
+from flask import Flask, request, render_template, session
+from flask import redirect
+from functools import wraps
+import os
+import json
 
 app = Flask(__name__)
+app.secret_key = "secretkey"
+app.config["UPLOADED_PHOTOS_DEST"] = "static"
 
-app.secret_key = 'secretkey'
-
+### end swagger specific ###
 books = [
     {
         "author": "Hernando de Soto",
         "country": "Peru",
         "language": "English",
         "pages": 209,
-        "title": "The Myster of Captial",
-        "year": 1970
-
+        "title": "The Mystery of Capital",
+        "year": 1970,
     },
     {
         "author": "Hans Christian Andersen",
         "country": "Denmark",
         "language": "Danish",
         "pages": 784,
-        "title": "Fairy Tales",
-        "year": 1836
-
+        "title": "Fairy tales",
+        "year": 1836,
     },
     {
         "author": "Dante Alighieri",
@@ -32,68 +33,81 @@ books = [
         "language": "Italian",
         "pages": 928,
         "title": "The Divine Comedy",
-        "year": 1315
-
-    }
+        "year": 1315,
+    },
 ]
-
-# _username = 'Faateh'
 
 users = [
-    {
-        "username": "testuser",
-        "password": "testuser"
-    }
-]
+            {"username": "testuser", "password": "testuser"},
+            {"username": "Faateh", "password": "Faateh"}
+         ]
 
-def checkuser(username, password):
-    for user in users:
-        if username in user["username"] and password in user["password"]:
-            return True
-    return False
+def sessionRequired(fn):
+    @wraps(fn)
+    def decorator(*args, **kwargs):
+        frombrowser = session["username"]
+        for user in users:
+            if session["username"] in frombrowser:
+                    return fn(*args, **kwargs)
 
+        return redirect("templates/register.html")
 
-# @app.route("/", methods=["GET"]) --old
-# def index():
-#     return render_template('index.html', username=_username)
+    return decorator
 
 @app.route("/", methods=["GET"])
-def home():
-    return render_template('register.html')
+def firstRoute():
+    return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        if checkuser(username, password):
-            response = make_response(
-                render_template('index.html', title = 'Books', books = books, username = username)
-            )
-            response.set_cookie('MySessionCookie', username)
-            return response  
+        user = {"username":request.form["username"], "password": request.form["password"]}
+
+        if user in users:
+            # set session data
+            session["username"] =  request.form["username"]
+            return render_template(
+                "index.html", username=session["username"], title = 'books', books = books)
         else:
-            return render_template('register.html')
-    
-    elif request.method =="GET":
-        return render_template('register.html')
+            return render_template("register.html")
+    else:
+        return render_template("register.html")
+
 
 @app.route("/logout")
 def logout():
-     # remove the username from the session if it is there
+    # remove the username from the session if it is there
     session.pop("username", None)
-    return "Logged Out of Books"
+    return "You are successfully logged out!"
+
 
 @app.route("/books", methods=["GET"])
-def get_books():
-    try:
-        user = request.cookies.get("MySessionCookie")
-        return render_template('books.html', books = books, username = user)
-    except:
-        return render_template('register.html')
+@sessionRequired
+def book():
+    username = session["username"]
+    return render_template(
+            "books.html", books=books, username=username, title="books"
+        )
 
 
-if __name__ == '__main__':
+@app.route("/addbook", methods=["GET", "POST"])
+@sessionRequired
+def addBook():
+    username = session["username"]
+    if request.method == "GET":
+        return render_template("addBook.html")
+    if request.method == "POST":
+        # expects pure json with quotes everywheree
+        new_book = request.form["book"]
+        myjson = json.loads(new_book)
+        books.append(myjson)
+        return render_template(
+            "books.html", books=books, username=username, title="books"
+        )
+    else:
+        return 400
+
+if __name__ == "__main__":
     app.run(debug=True)
-    # app.run(debug=True, host="0.0.0.0", port=5001)
+    # app.run(debug=True, host="0.0.0.0", port=5000)
